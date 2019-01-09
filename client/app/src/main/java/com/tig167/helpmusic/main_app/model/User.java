4a749f2189a5e16f88ececf0e5effea7b3c39fb6
Erik Boardy <gusboaer@student.gu.se>
2019-01-09 21:47:26 +0100

    added comments to user (client-side)

--- client/app/src/main/java/com/tig167/helpmusic/main_app/model/User.java
+++ client/app/src/main/java/com/tig167/helpmusic/main_app/model/User.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents a user of this application.
+ */
 public class User {
 
     private String name;
@@ -38,10 +41,20 @@ public class User {
 
     public void setProfileImage(Bitmap profileImage){ this.profileImage = profileImage; }
 
+    /**
+     * Accessor for friend data
+     *
+     * @return  an unmodifiable list of friends
+     */
     public List<User> friends() {
         return Collections.unmodifiableList(friends);
     }
 
+    /**
+     * Add a friend only if possible
+     *
+     * @param user  the user to be added to this friend data
+     */
     public void addFriend(User user) {
         if (!isUnfriendable(user)) {
             friends.add(user);
@@ -52,10 +65,23 @@ public class User {
         return friends.contains(user);
     }
 
+    /**
+     * Ensure that friend to be added is not this user
+     * and not already in the list of friends
+     *
+     * @param user  the user to be checked against this user
+     * @return      the boolean value of garbage data check, true on ok data
+     */
     public boolean isUnfriendable(User user) {
         return this.hasFriend(user) || this.equals(user);
     }
 
+    /**
+     * compare users by email
+     *
+     * @param other    the object to be compared with this user
+     * @return         the boolean value of email comparison, true on same email
+     */
     @Override
     public boolean equals(Object other) {
         if (!(other instanceof User)) {
