2864c8d60b2963fe18874174720e02426a59bf5c
Erik Boardy <gusboaer@student.gu.se>
2019-01-10 01:16:38 +0100

    fixed another typo in comment

--- client/app/src/main/java/com/tig167/helpmusic/data/local/Storage.java
+++ client/app/src/main/java/com/tig167/helpmusic/data/local/Storage.java
@@ -1,7 +1,7 @@
 package com.tig167.helpmusic.data.local;
 
 /**
- * Implemented by database helper class for local data handling, eg. save/load.
+ * Implemented by database helper class for local data handling, ie. save/load.
  *
  * @param <T> the object type handled by local data storage
  */
